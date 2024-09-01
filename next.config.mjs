f22314bbcc5b041b30beb3115dccb75d99b92a57
Simone D'Amilo <simodamilo@gmail.com>
2024-09-01 20:34:55 +0200

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-personal-website' : ''
 };
 
 export default withNextIntl(nextConfig);
