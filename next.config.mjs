66203525ec8ff9904f99eca9a30a0d74db3436f7
Simone D'Amilo <simodamilo@gmail.com>
2024-11-25 20:17:37 +0100

    Style configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,9 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-personal-website' : ''
+  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-personal-website' : '',
+  optimizeCss: false, 
+  enableBabelRuntime: true
 };
 
 export default withNextIntl(nextConfig);
