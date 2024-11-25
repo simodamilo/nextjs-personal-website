bd8c8b79253853a1e43dbb650588e41e93692d7f
Simone D'Amilo <simodamilo@gmail.com>
2024-11-25 20:25:37 +0100

    Added purge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
     "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: [
+    "./src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
